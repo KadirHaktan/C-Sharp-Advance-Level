bdae8249a2a5d5ec710d1ab75b4581053af01058
Ebubekir Doğan <ebubekir.dogan@bil.omu.edu.tr>
2018-05-25 13:00:53 -0800

    Object-Oriented
    Object-Oriented

---
+++ Fields/Kategori_Yoneticisi/Video.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kategori_Yoneticisi
+{
+    class Video
+    {
+        public void DetayGoster()
+        {
+            Kategori k = new Kategori();
+            // aynı assembly içinde oldugu için erişti. Normalde diğer projelerden erişimi yoktur.
+            k.Adi = "Ebubekir";
+            k.KarakterSayisi = 5;
+
+        }
+    }
+}
---
+++ Nesneler_ile_Calismak/Nesneler_ile_Calismak/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nesneler_ile_Calismak
+{
+    class Klip
+    {
+        public string SarkiciAdi;
+        public string SarkiAdi;
+        public string DosyaAdi;
+
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Kaç Klip nesnesi ile Çalışacaksiniz :");
+            string cevap = Console.ReadLine(); // Kullanicidan Değer almak için kullanılır.
+            int KlipAdet = int.Parse(cevap); // String to Int olayı
+
+        }
+    }
+}
