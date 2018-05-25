efec27353f436d074d8d2866ab23537577e6dcb3
Ebubekir Doğan <ebubekir.dogan@bil.omu.edu.tr>
2018-05-25 13:11:42 -0800

    Object-Oriented-CSharp-Console
    Object-Oriented-CSharp-Console

---
+++ Object-Oriented-CSharp-Console/Nesne_Kavramina_Giris/Nesne_Kavramina_Giris/Program.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nesne_Kavramina_Giris
+{
+    class Klip
+    {
+
+        public string SarkiciAdi;
+        public string SarkiAdi;
+        public string DosyaAdi;
+
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Int32 degisken = 5;
+
+            //Nesne
+            //Değişken
+            //Tip
+            //Sınıf
+
+            Double noktaliSayi = 4.5;
+            String kelime = "Software";
+            Boolean durum = true;
+            Char karakter = 'C';
+            ////////////////////////////////////////////////////
+            string dosyaAdi = "Ben Olsaydım.wmv";
+            string sarkici = "Mustafa Sandal";
+            string sarkiAdi = "Ben Olsaydım";
+
+            string dosyaAdi2 = "Hepsi Aşktan.wmv";
+            string sarkici2 = "Mustafa Sandal";
+            string sarkiAdi2 = "Hepsi Aşktan";
+
+            Console.WriteLine(sarkici);
+            
+
+            /// burası cok optimize değil en iyisi Class yapısı
+            ////////////////////////////////////////////////////
+
+            Klip k = new Klip(); // hafızaya bir klip nesnesi   
+            Console.WriteLine("*");
+            Console.WriteLine(k.SarkiAdi);
+            Console.WriteLine("*");
+
+            ////////////////////////////////////////////////////
+
+
+            Klip j1 = new Klip();
+            j1.SarkiAdi ="X";
+            j1.SarkiciAdi ="A";
+            j1.DosyaAdi ="F";
+
+            Klip j2 = new Klip();
+            j2.SarkiAdi = "Y";
+            j2.SarkiciAdi = "B";
+            j2.DosyaAdi = "F";
+
+            Klip j3 = new Klip();
+            j3.SarkiAdi = "Z";
+            j3.SarkiciAdi = "C";
+            j3.DosyaAdi = "F";
+
+            Console.WriteLine(j2.SarkiAdi);
+            ///////////////////////////////////////////////////////
+
+            int[] rakamlar; // Ortamda(bellekte) simdilik bir dizi yok. New ile türetmedik
+            rakamlar = new int[3];// new anahtar kelimesi ile 3 elemanlı bir int dizisi tanımlandı.
+
+            Console.WriteLine(rakamlar[0]);
+            Console.WriteLine(rakamlar[1]);
+            Console.WriteLine(rakamlar[2]);
+
+            ////////////// -> Şimdi Klip Dizisine geçiyoruz....
+
+            Klip[] klip = new Klip[3]; // Klip classının klip türünden olan 3 elemanlı bir Klip dizisi tanımlandı.
+            klip[0] = new Klip();
+            klip[0].SarkiAdi = "Bahçevan";
+            klip[0].SarkiciAdi = "Zeki Müren";
+            klip[0].DosyaAdi = "Bahcevan.wmv";
+
+            klip[1] = j1;
+            klip[2] = j2;
+
+            for (int i =0;i<klip.Length;i++)
+            {
+                Console.WriteLine(i+1 +  ". Sarki Adı : " + klip[i].SarkiAdi);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Object-Oriented-CSharp-Console/Null_Kavrami/Null_Kavrami/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Null_Kavrami
+{
+    class Ogrenci
+    {
+        public int OkulNo;
+        public string Ad;
+        public string Soyad;
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Ogrenci test; // default degeri null'dir.
+                          //  test.Ad = "Ebubekir"; // test degisken oldugu ver hafızada hiçbir ögrenci nesnesi olmadıgı için direk atama yapamayız.
+////////////////////////
+            Ogrenci denek = new Ogrenci();//Bu sekilde olur ama
+            denek.Ad = "Ebubekir"; //Bunu kabul etmesinin sebebi memoryde denek adlı nesne olusturuldugu için Ad soyad ve nosu belli olmayan bos birşey tanımaldık.
+
+///////////////////////////////
+            Ogrenci x = new Ogrenci();
+            x.Ad = "EBubekir";
+            Console.WriteLine(x.Ad[0]); // E harfi gelir bu sekildede ulaşabiliriz.
+            x = new Ogrenci(); // hafızada yeni nesne olusturuldu .
+            Console.WriteLine("-------------");
+            Console.WriteLine(x.Ad);
+
+///////////////////////////////////////////////////
+            Ogrenci s = new Ogrenci(); // yeni nesne tanımlandı.
+            s.OkulNo = 6599;
+            s.Ad = "Ceyhun";
+            s.Soyad = "asdasd";
+
+            Ogrenci f = s;
+            
+            Console.WriteLine(f.Ad);
+            Console.WriteLine(s.Ad);
+
+            f = new Ogrenci();  // yeni ögrenciyi işaret ediyor. s halen eski  Ceyhun adli ögrenciyi işaret ediyor.
+            f.Soyad = "Doğan";
+            Console.WriteLine(f.Ad);
+            s = f;
+            Console.WriteLine(f.Soyad);
+            Console.WriteLine(f.Ad); //boş satır üretir.
+
+            s = new Ogrenci();
+
+            s = null;
+            f = null; 
+
+            //////////////////////////////////////////
+            Console.ReadKey();
+        }
+    }
+}
