fad79be0e1ab2cacce57930008a980f03c03792e
Ebubekir Doğan <ebubekir.dogan@bil.omu.edu.tr>
2018-05-25 13:12:14 -0800

    Server-and-Client-Communication-C-Sharp
    Server-and-Client-Communication-C-Sharp

---
+++ Server-and-Client-Communication-C-Sharp/TCPIP/Form1.Designer.cs
@@ -0,0 +1,134 @@
+﻿namespace TCPIP_Demo
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.startButton = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtHost = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtPort = new System.Windows.Forms.TextBox();
+            this.stopButton = new System.Windows.Forms.Button();
+            this.txtStatus = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // startButton
+            // 
+            this.startButton.Location = new System.Drawing.Point(317, 28);
+            this.startButton.Name = "startButton";
+            this.startButton.Size = new System.Drawing.Size(75, 30);
+            this.startButton.TabIndex = 0;
+            this.startButton.Text = "Başla";
+            this.startButton.UseVisualStyleBackColor = true;
+            this.startButton.Click += new System.EventHandler(this.startButton_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(45, 28);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(49, 17);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Host : ";
+            // 
+            // txtHost
+            // 
+            this.txtHost.Location = new System.Drawing.Point(96, 27);
+            this.txtHost.Name = "txtHost";
+            this.txtHost.Size = new System.Drawing.Size(100, 22);
+            this.txtHost.TabIndex = 2;
+            this.txtHost.Text = "127.0.0.1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(202, 28);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(42, 17);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Port :";
+            // 
+            // txtPort
+            // 
+            this.txtPort.Location = new System.Drawing.Point(250, 28);
+            this.txtPort.Name = "txtPort";
+            this.txtPort.Size = new System.Drawing.Size(51, 22);
+            this.txtPort.TabIndex = 2;
+            this.txtPort.Text = "8910";
+            // 
+            // stopButton
+            // 
+            this.stopButton.Location = new System.Drawing.Point(398, 28);
+            this.stopButton.Name = "stopButton";
+            this.stopButton.Size = new System.Drawing.Size(75, 30);
+            this.stopButton.TabIndex = 3;
+            this.stopButton.Text = "Dur";
+            this.stopButton.UseVisualStyleBackColor = true;
+            this.stopButton.Click += new System.EventHandler(this.stopButton_Click);
+            // 
+            // txtStatus
+            // 
+            this.txtStatus.Location = new System.Drawing.Point(48, 64);
+            this.txtStatus.Multiline = true;
+            this.txtStatus.Name = "txtStatus";
+            this.txtStatus.Size = new System.Drawing.Size(425, 148);
+            this.txtStatus.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(500, 232);
+            this.Controls.Add(this.txtStatus);
+            this.Controls.Add(this.stopButton);
+            this.Controls.Add(this.txtPort);
+            this.Controls.Add(this.txtHost);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.startButton);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Server";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button startButton;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtHost;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtPort;
+        private System.Windows.Forms.Button stopButton;
+        private System.Windows.Forms.TextBox txtStatus;
+    }
+}
+
