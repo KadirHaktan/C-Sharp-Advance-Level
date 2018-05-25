165a77b5cb445f4cb5708ac4748f5b5363c65067
Ebubekir Doğan <ebubekir.dogan@bil.omu.edu.tr>
2018-05-25 12:59:22 -0800

    Design-Pattern
    Design-Pattern

---
+++ 2-FacadePattern/FasadePattern/Customer.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FasadePattern
+{
+    class Customer
+    {
+        public int CustomerNumber { get; set; }
+        public string TcNumber{ get; set; }
+        public string CustomerName { get; set; }
+    }
+}
---
+++ 4-AbstractFactoryPattern/AbstractFactoryPattern/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbstractFactoryPattern
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Factory factory = new Factory(new InterbaseFactory());
+            factory.Start();
+            Console.WriteLine("");
+            Factory factory2 = new Factory(new Db2Factory());
+            factory2.Start();
+        }
+    }
+}
