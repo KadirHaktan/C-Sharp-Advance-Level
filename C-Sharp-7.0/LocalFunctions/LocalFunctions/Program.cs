711283461a8373c237cf109d78514ee669224e7e
Ebubekir Doğan <ebubekir.dogan@bil.omu.edu.tr>
2018-06-25 03:53:22 -0800

    C-Sharp 7.0
    C-Sharp 7.0

---
+++ C-Sharp-7.0/LocalFunctions/LocalFunctions/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LocalFunctions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           // var add = new Func<int, int, int>((no1, no2) => no1 + no2); // iki sayının toplamını veren local function yazmıs olduk.
+           //eğer yukarıdaki olmasaydı asagıdaki gibi bir fonksiyon tanımı yapacaktık
+            var result = add(10,20);
+
+            Console.WriteLine($"Sum of 10 and 20 is : {result}");
+
+            int add(int s1,int s2)
+            {
+                return s1 + s2;
+            }
+        }
+    }
+}
---
+++ C-Sharp-7.0/RefLocal-RefReturns/RefLocal-RefReturns/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RefLocal_RefReturns
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] speakers = { "Ebubekir Doğan", "Furkan Isıtan", "Ahmet Ali" };
+
+            //ref string speaker = ref speakers[1];
+
+            ref string speaker = ref Find(speakers,"Ebubekir");
+            speaker += " Gates";
+
+            Console.WriteLine("Speaker Name : " + speaker);
+            Console.WriteLine("Speaker Data : \n" + String.Join(",", speakers));
+
+
+            //speaker = ref speakers[0]; Hata verir
+
+
+            List<string> authors = new List<string>() { "Ebubekir Doğan", "Furkan Isıtan", "Ahmet Ali" };
+
+            //ref string author = ref authors[1]; Buda hata verir
+
+            Array.Resize(ref speakers, 1);
+            speaker = "Dogan Ebubkeir";
+
+            Console.WriteLine("Speaker Name : " + speaker);
+            Console.WriteLine("Speaker Data : \n" + String.Join(",", speakers));
+ 
+        }
+        static ref string Find(string[] speakers, string speaker)
+        {
+            for (int i = 0; i < speakers.Length; i++)
+            {
+                if (speakers[i].Equals(speaker))
+                {
+                    return ref speakers[i];
+                }
+            }
+            // return ref "Speaker Not Found";  hata verir. hata fırlatmamız lazım asagıdaki gibi
+            throw new Exception("Speaker Not Found");
+
+        }
+    }
+}
